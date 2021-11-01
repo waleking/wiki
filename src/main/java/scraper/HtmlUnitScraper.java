0b1bdafc924c432d8735d4c1a0e55e092e03666d
waleking <huangwaleking@gmail.com>
2021-11-01 12:59:36 -0400

    Init project.
    1. compile the project by using "mvn package".
    2. run the main class by using "java -cp target/java-screen-scraper-1.0.jar com.mcnz.screen.scraper.HtmlUnitScraper"

---
+++ src/main/java/scraper/HtmlUnitScraper.java
@@ -0,0 +1,36 @@
+package com.mcnz.screen.scraper;
+
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HtmlUnitScraper {
+	
+	public static void main(String args[]) throws Exception {
+		
+	    String url = "https://www.theserverside.com/video/"
+	    		+ "Tough-sample-Git-and-GitHub-interview-"
+	    		+ "questions-and-answers";
+        System.out.println(url);
+	    WebClient webClient = new WebClient();
+	    webClient.getOptions().setUseInsecureSSL(true);
+		webClient.getOptions().setCssEnabled(false);
+	    webClient.getOptions().setJavaScriptEnabled(false);
+		
+
+		HtmlPage htmlPage = webClient.getPage(url);
+		System.out.println(htmlPage.getTitleText());
+
+		// DomNode domNode = htmlPage.querySelector("#author > div > a");	
+		DomNode domNode = htmlPage.querySelector("#author > div > ul > li > a");
+		System.out.println(domNode.asText());
+		
+		List<HtmlAnchor> anchors =  htmlPage.getAnchors();
+		for (HtmlAnchor anchor : anchors) {
+			System.out.println(anchor.getAttribute("href"));
+		}
+	}
+}
